ff652818c6ab0edae3b3b18d9a51fd2c2a63e7bd
Cameron Bosslaini <cameron@bossalini.net>
2021-02-04 23:09:28 -0600

    Wwise integration
    Created initial controller classes and integrated Wwise into project

---
+++ Capstone-Unity/Assets/Scripts/GenA 2.0/genAController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class genAController: MonoBehaviour
+{
+    // Global control variables
+    public float globalVolume = 0.4f;
+    public float volumeThresholdDistance = 10f;
+    public float volumeAttenuationFactor = 1.0f;
+
+    public List<Instrument> instruments = new List<Instrument>();
+    public List<InstrumentNode> instrumentNodes = new List<InstrumentNode>();
+    
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
